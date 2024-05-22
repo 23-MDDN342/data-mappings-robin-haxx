899d9f725a293a6554b82a45fb656250403d1f7a
Robbie <robbiek3214@gmail.com>
2024-05-22 20:54:29 +1200

    "draw stage" subfunction refactoring

--- face.js
+++ face.js
@@ -21,18 +21,7 @@ const stroke_color = [95, 52, 8];
 
 const drawHuman = false;// draws human mouth and nose elements 
 
-// example of a global function
-// given a segment, this returns the average point [x, y]
-function segment_average(segment) {
-  let sum_x = 0;
-  let sum_y = 0;
-  let s_len = segment.length;
-  for (let i=0; i<s_len; i++) {
-    sum_x = sum_x + segment[i][0];
-    sum_y = sum_y + segment[i][1];
-  }
-  return [sum_x / s_len , sum_y / s_len ];
-}
+ellipseMode(CENTER);
 
 // This where you define your own face object
 function Face() {
@@ -51,15 +40,32 @@ function Face() {
 
   this.draw = function(positions) {
 
-    ellipseMode(CENTER);
-
     let birdSpecies = Math.floor(this.bird_species);
-    // Downgrading from the approach I took to project 2 to just draw whatever I want for the different birds -
-    // I think this is a more suitable coding pattern for the amount of form variation I'm thinking of.
-    // one object-oriented system is enough :]
-    switch(birdSpecies){
-      case 0: //moa
+    // Changed to a function-oriented drawing approach, this gives me a lot of variability options since I don't want as many shared properties as project 2.
+    // This also keeps a registry of object-scoped variables and which stage of drawing they're used in.
 
+    let topLipMidY = positions.top_lip[4][1]
+    let btmLipMidY = positions.bottom_lip[4][1]
+    let diff = topLipMidY - btmLipMidY;
+
+    let left_eye_pos = segment_average(positions.left_eye);
+    let right_eye_pos = segment_average(positions.right_eye);
+
+    let curEyeShift = 0.04 * this.eye_shift;
+
+      this.drawHead(birdSpecies, positions);
+      this.drawBeak(birdSpecies, positions, topLipMidY,btmLipMidY,diff);
+      this.drawEyes(birdSpecies, positions,left_eye_pos,right_eye_pos);
+      this.drawJazz(birdSpecies, positions,topLipMidY,btmLipMidY,diff, curEyeShift)
+
+  }
+
+  // DRAWING SUBFUNCTIONS
+
+  this.drawHead = function(birdSpecies, positions){
+    switch(birdSpecies){
+      case 0:
+      
       // HEAD SHAPE
       stroke(stroke_color);
       strokeWeight(.01);
@@ -73,131 +79,124 @@ function Face() {
         ellipse(segment_average(positions.chin)[0], 0, 5- (i*.1), 4+-(i*.1));
         pop();
       };
+      
+      break;
+      default: //debug texture
+      debugOut("Head");
 
-      // BEAK
-      let largerMouth = this.mouth_size * 1.7;
-      let mouthInner = largerMouth * 0.9;
-
-      push();
-        translate(segment_average(positions.top_lip)[0], segment_average(positions.top_lip)[1]);
-        translate (-largerMouth *.5, 0);
-
-        let topLipMidY = positions.top_lip[4][1]
-        let btmLipMidY = positions.bottom_lip[4][1]
-        let diff = topLipMidY - btmLipMidY;
-
-        noStroke();
-        fill(beakColour);
-        // Outer "beak"
-        bezier (-1,0,
-          largerMouth*.2,-1.5,
-          largerMouth*.8,-1.5,
-          1+largerMouth,0
-        );
-        bezier (-1,0,
-          largerMouth*.2,1.5,
-          largerMouth*.8,1.5,
-          1+largerMouth,0
-        );
-        translate (0.5*(largerMouth - mouthInner), 0); // centers inner mouth region
-        fill(this.detailColour);
-        // Inner "mouth"
-        bezier
-              ( -1,0,
-                mouthInner*.3,topLipMidY + (1.5*diff),
-                mouthInner*.7,topLipMidY + (1.5*diff),
-                1+mouthInner,0
-              );
-        bezier
-              ( -1,0,
-                mouthInner*.3,0 - (diff),
-                mouthInner*.7,0 - (diff),
-                1+mouthInner,0
-              );
-
-      pop();
-      // segment_average(positions.top_lip)[0], segment_average(positions.top_lip)[1];
-      //ellipse(segment_average(positions.bottom_lip)[0], segment_average(positions.bottom_lip)[1], 1.36, 0.25 * this.mouth_size);
-
-      // eyebrows
-      fill( this.eyebrowColour);
-      stroke( this.eyebrowColour);
-      strokeWeight(0.04);
-
-      this.draw_segment(positions.left_eyebrow, 0.1 + diff*.15);
-      this.draw_segment(positions.left_eyebrow, -0.1 + diff*.25);
-
-      positions.right_eyebrow[2][1] += diff*.3;
-      this.draw_segment(positions.right_eyebrow, 0.1);
-      this.draw_segment(positions.right_eyebrow, -0.1);
-
-      // draw the chin segment using points
-      fill(this.chinColour);
-      stroke(this.chinColour);
-      if (drawHuman == true){
-        this.draw_segment(positions.chin);
-      }
-      fill(bothIslandsCol);
-      stroke(dryForestCol);
-      strokeWeight(0.01);
-      this.draw_segment(positions.nose_bridge);
-
-      if (drawHuman == true){
-        
-        this.draw_segment(positions.nose_tip);
     }
-      strokeWeight(0.03);
-
-      let left_eye_pos = segment_average(positions.left_eye);
-      let right_eye_pos = segment_average(positions.right_eye);
-
-      // eyes
-      //noStroke();
-      let curEyeShift = 0.04 * this.eye_shift;
-      if(this.num_eyes == 2) {
-        fill(this.detailColour);
-        noFill();
-        stroke
-        for(let i = 0; i < 10; i++){
-          push();
-            stroke(stroke_color);
-            strokeWeight(0.15);
-            ellipse(left_eye_pos[0], left_eye_pos[1], 1-(i*.1), 0.33);
-            ellipse(right_eye_pos[0], right_eye_pos[1], 1-(i*.1), 0.33);
-          pop();  
-          push();
-            //stroke(stroke_color);
-            strokeWeight(0.06);
-            ellipse(left_eye_pos[0], left_eye_pos[1], 0.4-(i*.15), 0.5);
-            ellipse(right_eye_pos[0], right_eye_pos[1], 0.4-(i*.15), 0.5);
-          pop();
-          
-        }
-
-      }
-      else {
-        let eyePosX = (left_eye_pos[0] + right_eye_pos[0]) / 2;
-        let eyePosY = (left_eye_pos[1] + right_eye_pos[1]) / 2;
-
-        fill(this.detailColour);
-        ellipse(eyePosX, eyePosY, 0.45, 0.27);
-
-        fill(this.mainColour);
-        ellipse(eyePosX - 0.1 + curEyeShift, eyePosY, 0.18);
-
-      }
-    break;
-    default:
-    fill(0)
-    ellipse(0,0, 0.5,0.5);
-    rect(-2,-2,4.5,4); 
-    console.log('oops, no visual representation for this birdSpecies value!');
-  } // closes switch statement
-
   }
 
 
-  // example of a function *inside* the face object.
+
+
+  this.drawBeak = function(birdSpecies, positions,topLipMidY,btmLipMidY,diff){
+          // BEAK
+          switch(birdSpecies){
+            case 0:
+          let largerMouth = this.mouth_size * 1.7;
+          let mouthInner = largerMouth * 0.9;
+    
+          push();
+            translate(segment_average(positions.top_lip)[0], segment_average(positions.top_lip)[1]);
+            translate (-largerMouth *.5, 0);
+    
+            noStroke();
+            fill(beakColour);
+            // Outer "beak"
+            bezier (-1,0,
+              largerMouth*.2,-1.5,
+              largerMouth*.8,-1.5,
+              1+largerMouth,0
+            );
+            bezier (-1,0,
+              largerMouth*.2,1.5,
+              largerMouth*.8,1.5,
+              1+largerMouth,0
+            );
+            translate (0.5*(largerMouth - mouthInner), 0); // centers inner mouth region
+            fill(this.detailColour);
+            // Inner "mouth"
+            bezier
+                  ( -1,0,
+                    mouthInner*.3,topLipMidY + (1.5*diff),
+                    mouthInner*.7,topLipMidY + (1.5*diff),
+                    1+mouthInner,0
+                  );
+            bezier
+                  ( -1,0,
+                    mouthInner*.3,0 - (diff),
+                    mouthInner*.7,0 - (diff),
+                    1+mouthInner,0
+                  );
+    
+          pop();
+
+          break;
+          default: //debug texture
+
+          debugOut("Beak");
+  }
+}
+
+this.drawEyes = function(birdSpecies,positions,left_eye_pos,right_eye_pos,curEyeShift){
+  push();
+    strokeWeight(0.03);
+      noFill();
+      stroke
+      for(let i = 0; i < 10; i++){
+        push();
+          stroke(stroke_color);
+          strokeWeight(0.15);
+          ellipse(left_eye_pos[0], left_eye_pos[1], 1-(i*.1), 0.33);
+          ellipse(right_eye_pos[0], right_eye_pos[1], 1-(i*.1), 0.33);
+        pop();  
+        push();
+          stroke(neutralCol);
+          strokeWeight(0.06);
+          ellipse(left_eye_pos[0], left_eye_pos[1], 0.4-(i*.15), 0.5);
+          ellipse(right_eye_pos[0], right_eye_pos[1], 0.4-(i*.15), 0.5);
+        pop();
+        
+      }
+
+
+  pop();
+}
+
+
+this.drawJazz = function(birdSpecies,positions,topLipMidY,btmLipMidY,diff){
+        // eyebrows
+        fill( this.eyebrowColour);
+        stroke( this.eyebrowColour);
+        strokeWeight(0.04);
+  
+        this.draw_segment(positions.left_eyebrow, 0.1 + diff*.15);
+        this.draw_segment(positions.left_eyebrow, -0.1 + diff*.25);
+  
+        positions.right_eyebrow[2][1] += diff*.3;
+        this.draw_segment(positions.right_eyebrow, 0.1);
+        this.draw_segment(positions.right_eyebrow, -0.1);
+  
+        // draw the chin segment using points
+        fill(this.chinColour);
+        stroke(this.chinColour);
+        if (drawHuman == true){
+          this.draw_segment(positions.chin);
+        }
+        fill(bothIslandsCol);
+        stroke(dryForestCol);
+        strokeWeight(0.01);
+        this.draw_segment(positions.nose_bridge);
+  
+        if (drawHuman == true){
+          
+          this.draw_segment(positions.nose_tip);
+      }
+}
+
+  // DRAW POSITION FUNCTIONS
+
   // this draws a segment, and do_loop will connect the ends if true
   this.draw_segment = function(segment, y_mod = 0, do_loop) {
     for(let i=0; i<segment.length; i++) {
@@ -241,6 +240,8 @@ function Face() {
     }
   };
 
+  // VALUE MAPPING FUNCTIONS
+
   /* set internal properties based on list numbers 0-100 */
   this.setProperties = function(settings) {
     this.num_eyes = int(map(settings[0], 0, 100, 1, 2));
@@ -258,4 +259,34 @@ function Face() {
     settings[3] = map(this.bird_species, 0, 4, 0, 100);
     return settings;
   }
+}
+
+// RELEVANT GLOBAL FUNCTIONS
+
+// given a segment, this returns the average point [x, y]
+function segment_average(segment) {
+  let sum_x = 0;
+  let sum_y = 0;
+  let s_len = segment.length;
+  for (let i=0; i<s_len; i++) {
+    sum_x = sum_x + segment[i][0];
+    sum_y = sum_y + segment[i][1];
+  }
+  return [sum_x / s_len , sum_y / s_len ];
+}
+
+// DEBUG: this draws a "missing texture" grid and prints a debug message for the draw sub-function call that failed 
+// (called only in the default case for each draw stage with the relevant stage name passed in as string)
+function debugOut(drawStage){
+  fill(0)
+  //ellipse(0,0, 0.5,0.5);
+  rect(-2,-2,4,4); 
+  fill (255,0,255);
+  for (let i = -4; i < 4; i++){
+    for (let j = -4; j < 4; j++){
+      rect(i/2,j/2, 0.25);
+      rect(i/2+(.25),j/2+(.25), 0.25);
+    }
+  }
+console.log("OUTPUT: " + drawStage + " debug'");
 }
